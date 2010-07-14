d45c72452d97e7385a6bbc9eae07eefcfbca0b73
Simon Cozens <simon@simon-cozens.org>
2010-07-14 15:01:47 +0100

    I suppose you'll be wanting, like, documentation and stuff.

--- lib/Net/ResellerClub.pm
+++ lib/Net/ResellerClub.pm
@@ -10,7 +10,38 @@ my %methods;
 for my $c (grep /Service$/, $finder->plugins) { 
     push @{$methods{$_}}, $c for keys %{$c->_methods};
 }
-#die Dumper\%methods;
+
+=head1 NAME
+
+Net::ResellerClub - Perlish interface to ResellerClub's SOAP API
+
+=head1 VERSION
+
+Version 1.00
+
+=head1 SYNOPSIS
+
+    use Net::ResellerClub;
+
+    my $rc = Net::ResellerClub->new(
+        environment => "test",
+        username => $username,
+        password => $password,
+        parent   => '999999998');
+
+    $rc->DomOrder_list(@args); # call DomOrder.list
+
+    $rc->DomOrder_cancelTransfer(1234); 
+    # If the method name is unambiguous across classes, you can call it
+    # directly:
+    $rc->cancelTransfer(1234); 
+
+=head1 FUNCTIONS
+
+Ah, you'll want to be looking at the ResellerClub API Documentation
+(such as it is) for those.
+
+=cut
 
 sub new {
     my ($self, %args) = @_;
@@ -62,5 +93,36 @@ sub _call {
 
     return DirectIXMLIO::ParseXML($som->result);
 }
+
+=head1 AUTHOR
+
+Simon Cozens, C<< <simon at cpan.org> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to Github
+L<http://github.com/simoncozens/Net-ResellerClub/issues>. I will be notified,
+and then you'll automatically be notified of progress on your bug as I make
+changes.
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc Net::DomainRegistration::Simple
+
+=head1 ACKNOWLEDGEMENTS
+
+Thanks to the UK Free Software Network (http://www.ukfsn.org/) for their
+support of this module's development. For free-software-friendly hosting
+and other Internet services, try UKFSN.
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2010 Simon Cozens.
+
+This program is released under the following license: Perl
+
+=cut
 
 1;
